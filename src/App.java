a59fdecf76f78a33966c522878a0d3ca0e7afe9e
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2023-09-13 10:42:49 +0200

    lek 4 uppg 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        Scanner roblox = new Scanner(System.in);
+        System.out.println("Skriv text");
+        String text = roblox.nextLine();
+
+        int antal_bokstäver = text.length();
+
+        System.out.println("\""+text+"\" har " +antal_bokstäver+ " bokstäver");
     }
 }
