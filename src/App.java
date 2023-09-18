bce0256600e64256a62de81a57fc002b96891bf6
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2023-09-18 08:39:10 +0200

    har skapat ett program som lokaliserar vart en boktsav befinner sig

--- src/App.java
+++ src/App.java
@@ -8,6 +8,14 @@ public class App {
 
         int antal_bokstäver = text.length();
 
+        System.out.println("skriv en bokstav");
+
+        String bokstav = roblox.nextLine();
+
+        int plats = text.indexOf(bokstav);
+
         System.out.println("\""+text+"\" har " +antal_bokstäver+ " bokstäver");
+
+        System.out.println("bokstaven "+bokstav+ " är på plats " +(plats+1));
     }
 }
